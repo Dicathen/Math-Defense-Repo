717dc60d7facf89f7ce89338f1edb669cecc2047
hoalex219 <hoalex219@gmail.com>
2019-11-05 14:31:49 -0600

    11/5/19 Commit

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using TMPro;
 using UnityEngine;
 using static PathCreator;
 
@@ -9,36 +10,39 @@ public class Spawner : MonoBehaviour
 {
     public Transform spawnPos;
     public GameObject[] spwnee;
-
-    private float timeSeperation = 1;
+    
+   
+    private float timeSeperation = .5f;
     private int spawnlet = 0;
     private Boolean letSpawn = false;
 
     // Update is called once per frame
+    private void Start()
+    {
+        
+    }
+
     void Update()
     {
-        //PathCreator bob = new PathCreator();
+        letSpawn = true;
 
         if (letSpawn == true)
         {
             if (timeSeperation <= 0)
             {
                 Instantiate(spwnee[spawnlet], spawnPos.position, spawnPos.rotation);
-                timeSeperation = 1f;
+                timeSeperation = .5f;
             }
 
             timeSeperation -= Time.deltaTime;
         }
         
-        if (Time.timeSinceLevelLoad > 14)
-        {
-            letSpawn = true;
-        }
 
-        if (Time.timeSinceLevelLoad > 74)
+            if (Time.timeSinceLevelLoad > 74)
         {
             spawnlet = 1;
         }
+        
     }
     
 }
